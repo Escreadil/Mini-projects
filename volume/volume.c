fed0faf71044312efb3447ea067058ccc6a3c48e
Escreadil <143181618+Escreadil@users.noreply.github.com>
2024-01-16 00:44:30 +0300

    Update volume.c

--- volume/volume.c
+++ volume/volume.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 
-// Number of bytes in .wav header
+
 const int HEADER_SIZE = 44;
 
 int main(int argc, char *argv[])
@@ -35,13 +35,13 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    
     uint8_t header[HEADER_SIZE];
 
     fread(header, sizeof(int), 1, input);
     fwrite(header, sizeof(int), 1, output);
 
-    // TODO: Read samples from input file and write updated data to output file
+    
     int16_t buffer;
     while (fread(&buffer, sizeof(buffer), 1 , input))
     {
